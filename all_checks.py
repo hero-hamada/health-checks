246d8744f4e6a6b4b4b36016f499121a442d8681
hero-hamada <57315804+hero-hamada@users.noreply.github.com>
2020-07-13 15:31:04 +0300

    Update all_checks.py

--- all_checks.py
+++ all_checks.py
@@ -4,10 +4,15 @@ import sys
 def ch_reboot():
 	return os.path.exists("/run/reboot-required")
 
+def check_disk():
+	return True
+
 def main():
 	if ch_reboot():
 		print("Pending reboot.")
 		sys.exit(1)
+	if check_disk():
+		print("Good")
 	print("ev ok:")
 	sys.exit(0)
 main()
