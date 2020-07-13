00f59d34d355ce88cd698a83e04194769a4c4804
Hero <maria.zhan99@gmail.com>
2020-07-13 15:38:04 +0300

    Diskfull f

--- all_checks.py
+++ all_checks.py
@@ -4,10 +4,15 @@ import sys
 def ch_reboot():
 	return os.path.exists("/run/reboot-required")
 
+def check_diskfull():
+	pass
+
 def main():
 	if ch_reboot():
 		print("Pending reboot.")
 		sys.exit(1)
 	print("ev ok:")
+
 	sys.exit(0)
+
 main()
